acf60bcfdc0816b3a9bf8968156f1d619e612b14
nikan48g <nikan@bisnor.app>
2026-09-08 19:57:24 +0330

    Release v4.5.0: Player lock overhaul, PiP removal, sleek timebar, search tab rename & glassmorphic nav polish

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.hnn.bisnor"
         minSdk = 24
         targetSdk = 35
-        versionCode = 7
-        versionName = "4.0.0"
+        versionCode = 8
+        versionName = "4.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
